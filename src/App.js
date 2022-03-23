99692426f35deab29f93e578d7d85e9b0e960650
Simon Vajda <simon.vajda23@gmail.com>
2022-03-23 21:34:40 +0100

    BrowserRouter added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Switch } from "@mui/material";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import SignUp from "./components/SignUp";
 
 function App() {
-  return <SignUp></SignUp>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUp></SignUp>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
