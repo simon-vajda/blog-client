b7d40a2a7fb69af4c701da18e29892fb6afaf1e6
Simon Vajda <simon.vajda23@gmail.com>
2022-04-03 00:55:24 +0200

    Backend url based on NODE_ENV value

--- src/ApiConfig.js
+++ src/ApiConfig.js
@@ -1,6 +1,7 @@
-export const API_URL = process.env
-  ? "http://localhost:8080/api/v1"
-  : "https://blog-server.azurewebsites.net/api/v1";
+export const API_URL =
+  process.env?.NODE_ENV === "development"
+    ? "http://localhost:8080/api/v1"
+    : "https://blog-server.azurewebsites.net/api/v1";
 
 export const headers = (user) => ({
   Authorization: user ? `Bearer ${user.token}` : "",
