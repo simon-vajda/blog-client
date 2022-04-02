45fe7fbcb05ebab73615f28fba22973771de4e21
Simon Vajda <simon.vajda23@gmail.com>
2022-04-03 00:32:37 +0200

    correct api url is selected based on the environment

--- src/ApiConfig.js
+++ src/ApiConfig.js
@@ -1,5 +1,6 @@
-export const API_URL =
-  process.env.REACT_APP_API_URL ?? "http://localhost:8080/api/v1";
+export const API_URL = process.env
+  ? "http://localhost:8080/api/v1"
+  : "https://blog-server.azurewebsites.net/api/v1";
 
 export const headers = (user) => ({
   Authorization: user ? `Bearer ${user.token}` : "",
