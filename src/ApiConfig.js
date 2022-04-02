b649049a971e02925e29640d7354ea36450214a1
Simon Vajda <simon.vajda23@gmail.com>
2022-04-02 18:38:55 +0200

    Api url can be set with an environment variable

--- src/ApiConfig.js
+++ src/ApiConfig.js
@@ -1,4 +1,5 @@
-export const API_URL = "http://localhost:8080/api/v1";
+export const API_URL =
+  process.env.REACT_APP_API_URL ?? "http://localhost:8080/api/v1";
 
 export const headers = (user) => ({
   Authorization: user ? `Bearer ${user.token}` : "",
