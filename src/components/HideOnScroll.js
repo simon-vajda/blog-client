3ab2502b6e64f85d956d293fbcf4d3b79deef93c
Simon Vajda <simon.vajda23@gmail.com>
2022-04-06 22:32:38 +0200

    HideOnScroll implemented on AppBar, and logout icon added.

---
+++ src/components/HideOnScroll.js
@@ -0,0 +1,12 @@
+import { Slide, useScrollTrigger } from "@mui/material";
+
+export default function HideOnScroll(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
--- src/components/MenuAppBar.js
+++ src/components/MenuAppBar.js
@@ -5,14 +5,18 @@ import {
   Button,
   IconButton,
   Link,
+  ListItemIcon,
+  ListItemText,
   Menu,
   MenuItem,
   Toolbar,
   Typography,
 } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
 import { useContext, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { UserContext } from "../UserContext";
+import HideOnScroll from "./HideOnScroll";
 
 export default function MenuAppBar() {
   const navigate = useNavigate();
@@ -34,58 +38,64 @@ export default function MenuAppBar() {
   };
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Box sx={{ flexGrow: 1 }}>
-          <Link
-            to="/"
-            variant="h6"
-            display="inline"
-            color="inherit"
-            underline="hover"
-            component={NavLink}
-          >
-            Blog
-          </Link>
-        </Box>
-        {currentUser ? (
-          <div>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
+    <HideOnScroll>
+      <AppBar position="sticky">
+        <Toolbar>
+          <Box sx={{ flexGrow: 1 }}>
+            <Link
+              to="/"
+              variant="h6"
+              display="inline"
               color="inherit"
+              underline="hover"
+              component={NavLink}
             >
-              <Typography mr={1}>{currentUser.name}</Typography>
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem>Profile</MenuItem>
-              <MenuItem onClick={handleLogout}>Log out</MenuItem>
-            </Menu>
-          </div>
-        ) : (
-          <Button color="inherit" component={NavLink} to="/login">
-            Login
-          </Button>
-        )}
-      </Toolbar>
-    </AppBar>
+              Blog
+            </Link>
+          </Box>
+          {currentUser ? (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <Typography mr={1}>{currentUser.name}</Typography>
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleLogout}>
+                  <ListItemIcon>
+                    <LogoutIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Log out</ListItemText>
+                </MenuItem>
+              </Menu>
+            </div>
+          ) : (
+            <Button color="inherit" component={NavLink} to="/login">
+              Login
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+    </HideOnScroll>
   );
 }
