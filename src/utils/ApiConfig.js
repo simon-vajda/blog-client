392e2debce86ca9e51348e68a16e1c6839ee385d
Simon Vajda <simon.vajda23@gmail.com>
2022-04-07 22:17:47 +0200

    Api hostname is same as current when dev profile is active

--- src/utils/ApiConfig.js
+++ src/utils/ApiConfig.js
@@ -1,6 +1,6 @@
 export const API_URL =
   process.env?.NODE_ENV === "development"
-    ? "http://localhost:8080/api/v1"
+    ? `http://${window.location.hostname}:8080/api/v1`
     : "https://blog-server.azurewebsites.net/api/v1";
 
 export const headers = (user) => ({
