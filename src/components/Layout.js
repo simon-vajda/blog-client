448f748ae953e8a905060e44a97ac1da00e3e7f7
Simon Vajda <simon.vajda23@gmail.com>
2022-04-09 01:05:09 +0200

    Footer removed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,10 @@
-import { Box } from "@mui/material";
-import Footer from "./Footer";
 import MenuAppBar from "./MenuAppBar";
 
 export function Layout(props) {
   return (
-    <Box
-      minHeight="100vh"
-      display="flex"
-      flexDirection="column"
-      justifyContent="space-between"
-    >
+    <>
       <MenuAppBar></MenuAppBar>
-      <Box flexGrow={1}>{props.children}</Box>
-      <Footer></Footer>
-    </Box>
+      {props.children}
+    </>
   );
 }
