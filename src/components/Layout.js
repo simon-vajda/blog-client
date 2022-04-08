b13cb4721412fa09c01db63ddf1b2262740b7c50
Simon Vajda <simon.vajda23@gmail.com>
2022-04-08 13:36:00 +0200

    Footer added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,18 @@
+import { Box } from "@mui/material";
+import Footer from "./Footer";
 import MenuAppBar from "./MenuAppBar";
 
 export function Layout(props) {
   return (
-    <>
+    <Box
+      minHeight="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-between"
+    >
       <MenuAppBar></MenuAppBar>
-      {props.children}
-    </>
+      <Box flexGrow={1}>{props.children}</Box>
+      <Footer></Footer>
+    </Box>
   );
 }
